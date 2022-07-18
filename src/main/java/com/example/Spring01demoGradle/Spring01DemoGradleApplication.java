f3db42c0ab2960f4b53d68c119a3e99218de1c8c
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-18 14:00:37 +0200

    CREATION

---
+++ src/main/java/com/example/Spring01demoGradle/Spring01DemoGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring01demoGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring01DemoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring01DemoGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring01demoGradle/Spring01DemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring01demoGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01DemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
